36c6f21d92df9a01a38d4d005d1061c0b2443523
Dankiest Raft <daniel.yahalom@gmail.com>
2018-03-14 10:52:46 -0500

    substancially improved jumping Although the new code might need a little cleaning and re-organizing

--- la voyage dans la lune/Assets/movement.cs
+++ la voyage dans la lune/Assets/movement.cs
@@ -4,54 +4,81 @@ using UnityEngine;
 
 public class movement : MonoBehaviour {
 
-	void Start()
-	{
-		playerRigidbody = GetComponent<Rigidbody2D>();
-	}
 
 	//getting player input
 	void Update()
 	{
-		if(Input.GetKeyDown("space") && GetComponentInChildren<legCollider>().isGrounded)
+		if(Input.GetKeyDown("space") && isGrounded)
 			startJumping();
 		
 		if(isJumping)
 			movementAxis = Input.GetAxis("Horizontal");
 		else
 			movementAxis = Input.GetAxisRaw("Horizontal");
+
+		jumpCurve = generateCurve();
 	}
 
-	Rigidbody2D playerRigidbody;
-	[SerializeField] public AnimationCurve jumpCurve;
 	[SerializeField] public float speed;
 	float movementAxis;
-	[SerializeField] public float gravity;
-
 	void FixedUpdate () {
-		//jumping
-		if(isJumping)
+		GetComponent<Rigidbody2D>().drag = 0;
+		isGrounded = GetComponentInChildren<legCollider>().isGrounded;
+		if(!isGrounded && !isJumping)
 		{
-			jumpDistance = jumpCurve.Evaluate(jumpFrame/(jumpTime*60)) - jumpCurve.Evaluate((jumpFrame - 1)/(jumpTime*60));
-			jumpFrame++;
-			if(jumpCurve.keys[jumpCurve.length - 1].time < (jumpFrame/(jumpTime*60))) //if jump is over (determined by wether the jumpFrame is bigger)
-			{
-				isJumping = false;
-				jumpDistance = 0;
-			}
-			playerRigidbody.velocity = new Vector2(movementAxis * speed, jumpDistance * 60);
-		} else
-			playerRigidbody.velocity = new Vector2(movementAxis * speed, -gravity);
+			isFalling = true;
+			
+		}
+		if(isJumping|| isFalling)
+		{
+			Debug.Log("falltime:" + fallTime);
+			gravity = evaluateDelta(fallTime, (fallTime + 1));
+			Debug.Log(gravity);
+			fallTime += 1;
+		}
+		if(isGrounded && (!isJumping || fallTime > 10))
+		{
+			fallTime = jumpVertex.x;
+			isJumping = false;
+			isFalling = false;
+			gravity = 0;
+		}
+		GetComponent<Rigidbody2D>().velocity = new Vector2(movementAxis * speed, gravity * 60); //we are multiplying by 60 to convert gravity from m/sec m/frame
 
 	}
 
-	float jumpDistance;
-	[SerializeField] bool isJumping = false;
-	[SerializeField] int jumpFrame;
-	float jumpTime = 1;
-	public void startJumping()
+	public bool isGrounded;
+	public bool isJumping;
+	public bool isFalling;
+	[SerializeField] AnimationCurve jumpCurve;
+	[SerializeField] float gravity;
+	public Vector2 jumpVertex;
+	[SerializeField] float fallTime;
+
+	void startJumping()
 	{
-		jumpDistance = 0;
+		Debug.Log("jump Started");
 		isJumping = true;
-		jumpFrame = 0;
+		fallTime = 0;
+	}
+
+	AnimationCurve generateCurve()
+	{
+		AnimationCurve newCurve = new AnimationCurve();
+		newCurve.AddKey(0,0);
+		newCurve.AddKey(jumpVertex.x, jumpVertex.y);
+		newCurve.AddKey(jumpVertex.x*2, 0);
+		return newCurve;
+	}
+
+	float evaluateDelta(float firstValue, float secondValue)
+	{
+		return evaluateCurve(jumpVertex ,secondValue) - evaluateCurve(jumpVertex ,firstValue);
+	}
+
+	float evaluateCurve(Vector2 vertex, float point)
+	{
+		return (-vertex.y/(vertex.x*vertex.x))*(point-vertex.x)*(point - vertex.x) + vertex.y;
 	}
 }
+
