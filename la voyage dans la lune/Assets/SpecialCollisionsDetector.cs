6455253a047356d8ba1c243f176b52fa9a6192c2
Dankiest Raft <daniel.yahalom@gmail.com>
2018-03-16 21:36:05 -0500

    updated to 2017.3.1

---
+++ la voyage dans la lune/Assets/SpecialCollisionsDetector.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpecialCollisionsDetector : MonoBehaviour {
+
+	// Use this for initialization
+	void OnCollisionEnter2D(Collision2D col)
+	{
+		if(col.contacts[0].p)
+	}
+}
